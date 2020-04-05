cece3c3b1dd929abdb051308a1d167822ec2f42a
TurBoss <j.l.toledano.l@gmail.com>
2020-04-05 03:10:37 +0200

    add files

--- TurBoMonitor3000.py
+++ TurBoMonitor3000.py
@@ -3,25 +3,22 @@ import os
 import psutil
 import serial
 from time import sleep
-from threading import Thread
 
-if os.name == "posix":
-    import sensors
+import status_pb2
 
-if os.name == "nt":
-    PORT = "COM4"
-elif os.name == "posix":
-    PORT = "/dev/ttyACM0"
-    os.nice(0)
+import sensors
+
+PORT = "/dev/ttyACM0"
+os.nice(0)
 
 
-class Monitor():
+class Monitor:
     def __init__(self):
         """Initialize"""
         print("Initialize")
 
-        if os.name == "posix":
-            sensors.init()
+        self.metrics = status_pb2.CpuStatus()
+        sensors.init()
 
         self.ser = serial.Serial()
         self.ser.baudrate = 9600
@@ -29,11 +26,10 @@ class Monitor():
 
         try:
             self.ser.open()
-        except:
-            print("no serial port")
+        except Exception as e:
+            print("no serial port {}".format(e))
 
-        if os.name == "posix":
-            self.chips = [[0 for feature in chip] for chip in sensors.iter_detected_chips()]
+        self.chips = [[0 for feature in chip] for chip in sensors.iter_detected_chips()]
 
         self.cores = psutil.cpu_count()
         self.cpuTotal = psutil.cpu_percent()
@@ -47,155 +43,74 @@ class Monitor():
         self.lastnetdw = 0
         self.interval = 1
 
+    def __del__(self):
+        sensors.cleanup()
+
     def update(self):
-        while True:
-            """Update Data"""
-            if os.name == "posix":
-                i = 0
-                for chip in sensors.iter_detected_chips():
-                    j = 0
-                    for feature in chip:
-                        self.chips[i][j] = feature.get_value()
-                        j += 1
-                    i += 1
+        """Update Data"""
+        i = 0
+        for chip in sensors.iter_detected_chips():
+            j = 0
+            for feature in chip:
+                self.chips[i][j] = feature.get_value()
+                j += 1
+            i += 1
 
-            cpuTotal = psutil.cpu_percent()
-            self.cpuTotal = str(cpuTotal)
-            cpu = psutil.cpu_percent(percpu=True)
-            for i in range(self.cores):
-                cpu[i] = str(cpu[i])
-                if len(cpu[i]) == 3:
-                    cpu[i] = cpu[i] + "0 "
-                elif len(cpu[i]) == 4:
-                    cpu[i] = cpu[i] + " "
-                self.cpu[i] = cpu[i]
+        self.cpuTotal = psutil.cpu_percent()
+        self.cpu = psutil.cpu_percent(percpu=True)
 
-            self.mem = psutil.virtual_memory()
-            self.disk = psutil.disk_usage('/')
+        self.mem = psutil.virtual_memory()
+        self.disk = psutil.disk_usage('/')
 
-            self.netio = psutil.net_io_counters()
+        self.netio = psutil.net_io_counters()
 
-            self.netup = self.netio.bytes_sent - self.lastnetup
-            self.netdw = self.netio.bytes_recv - self.lastnetdw
+        self.netup = self.netio.bytes_sent - self.lastnetup
+        self.netdw = self.netio.bytes_recv - self.lastnetdw
 
-            self.lastnetup = self.netio.bytes_sent
-            self.lastnetdw = self.netio.bytes_recv
+        self.lastnetup = self.netio.bytes_sent
+        self.lastnetdw = self.netio.bytes_recv
 
-            self.netup = str(self.netup / 1000)
-            self.netdw = str(self.netdw / 1000)
-
-            if len(self.netup) == 1:
-                self.netup = self.netup + "  "
-            if len(self.netup) == 2:
-                self.netup = self.netup + " "
-
-            if len(self.netdw) == 1:
-                self.netdw = self.netdw + "  "
-            if len(self.netdw) == 2:
-                self.netdw = self.netdw + " "
-            if len(self.netdw) == 3:
-                self.netdw = self.netdw + " "
-
-            sleep(1)
-
-
-            # i = 0
-            # for p in psutil.pids():
-        #	proc = psutil.Process(p)
-        #	print p , proc.name(), proc.cpu_percent()
-        #	i += 1
+        self.netup = self.netup / 1000
+        self.netdw = self.netdw / 1000
 
     def send(self):
-        while True:
-            """Send Data"""
-            sleep(1)
+        """Send Data"""
 
-            # print"Cores =",self.cores
-            self.ser.write("CORE {0} \0\n".format(str(self.cores)).encode("UTF-8"))
-            sleep(0.1)
+        self.metrics.cores = int(self.cores)
 
-            # print"1.5v = ",self.chips[0][0]
-            self.ser.write("1V {0} \0\n".format(str(self.chips[0][0])))
-            sleep(0.1)
+        self.metrics.cpuTotal = float(self.cpuTotal)
 
-            # print"3.3v = ",self.chips[0][1]
-            self.ser.write("3V {0} \0\n".format(str(self.chips[0][1])))
-            sleep(0.1)
+        self.metrics.core0 = float(self.cpu[0])
+        self.metrics.core1 = float(self.cpu[1])
+        self.metrics.core2 = float(self.cpu[2])
+        self.metrics.core3 = float(self.cpu[3])
+        self.metrics.core4 = float(self.cpu[4])
+        self.metrics.core5 = float(self.cpu[5])
+        self.metrics.core6 = float(self.cpu[6])
+        self.metrics.core7 = float(self.cpu[7])
+        self.metrics.core8 = float(self.cpu[8])
+        self.metrics.core9 = float(self.cpu[9])
+        self.metrics.core10 = float(self.cpu[10])
+        self.metrics.core11 = float(self.cpu[11])
 
-            # print"5.0v = ",self.chips[0][2]
-            self.ser.write("5V {0} \0\n".format(str(self.chips[0][2])))
-            sleep(0.1)
+        self.metrics.mem_percent = self.mem.percent
+        self.metrics.mem_total = self.mem.total/1000000
+        self.metrics.mem_available = self.mem.available/1000000
+        self.metrics.disk_percent = self.disk.percent
+        self.metrics.net_up = self.netup/1000
+        self.metrics.net_dw = self.netdw/1000
 
-            # print"12.0v = ",self.chips[0][3]
-            self.ser.write("12V {0} \0\n".format(str(self.chips[0][3])))
-            sleep(0.1)
-
-            # print"CPUFAN = ",self.chips[0][4]
-            self.ser.write("CPUFAN {0} \0\n".format(str(self.chips[0][4])))
-            sleep(0.1)
-
-            # print"CPUTEMP = ",self.chips[0][5]
-            self.ser.write("CPUTEMP {0} \0\n".format(str(self.chips[0][5])))
-            sleep(0.1)
-
-            # print"MBTEMP = ",self.chips[0][6]
-            self.ser.write("MBTEMP {0} \0\n".format(str(self.chips[0][6])))
-            sleep(0.1)
-
-            # print"GPUTEMP = ",self.chips[1][0]
-            self.ser.write("GPUTEMP {0} \0\n".format(str(self.chips[1][0])))
-            sleep(0.1)
-
-            # print"CPU =",self.cpuTotal
-            self.ser.write("CPUTOTAL {0} \0\n".format(str(self.cpuTotal)))
-            sleep(0.1)
-
-            for i in range(self.cores):
-                # print"C"+str(i+1)+" = "+str(self.cpu[i])
-                self.ser.write("CPU {0} = {1} \0\n".format(str(i + 1), str(self.cpu[i])))
-                sleep(0.1)
-
-            # print "MEMPERC = "+str(self.mem.percent)
-            self.ser.write("MEMPERC {0} \0\n".format(str(self.mem.percent)))
-            sleep(0.1)
-
-            # print "MEMTOTAL = "+str(self.mem.total/1000000)
-            self.ser.write("MEMTOTAL {0} \0\n".format(str(self.mem.total / 1000000)))
-            sleep(0.1)
-
-            # print "MEMAVAIL = "+str(self.mem.available/1000000)
-            self.ser.write("MEMAVAIL {0} \0\n".format(str(self.mem.available / 1000000)))
-            sleep(0.1)
-
-            # print "DISK = "+str(self.disk.percent)
-            self.ser.write("DISK {0} \0\n".format(str(self.disk.percent)))
-            sleep(0.1)
-
-            # print "NET UP = "+str(self.netup/1000)
-            self.ser.write("NETUP {0} \0\n".format(str(self.netup)))
-            sleep(0.1)
-
-            # print "NET DOWN = "+str(self.netdw/1000)
-            self.ser.write("NETDW {0} \0\n".format(str(self.netdw)))
-            sleep(0.1)
+        # print(self.metrics.SerializeToString())
+        self.ser.write(self.metrics.SerializeToString())
 
 
 def main():
     monitor = Monitor()
 
-    updateThread = Thread(target=monitor.update)
-    sendThread = Thread(target=monitor.send)
-
-    updateThread.daemon = True
-    sendThread.daemon = True
-
-    updateThread.start()
-    sendThread.start()
-
-    while (1):
-        sleep(1000)
-
-    sensors.cleanup()
+    while True:
+        monitor.update()
+        monitor.send()
+        sleep(0.1)
 
 
 if __name__ == '__main__':
--- TurBoMonitor3000/TurBoMonitor3000.ino
+++ TurBoMonitor3000/TurBoMonitor3000.ino
@@ -1,511 +1,123 @@
-#include <LiquidCrystal_SR_LCD3.h>
+#include "status.pb.h"
+#include <pb_common.h>
+#include <pb.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
 
-LiquidCrystal_SR_LCD3 lcd(11, 10, 12);
+#include "SPI.h"
+#include "ILI9341_t3.h"
 
-LCD *screen = &lcd;
+#define TFT_DC  9
+#define TFT_CS 10
 
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
-const int analogInPin1 = A0;  // Analog input pin that the potentiometer is attached to
 
-int sensorValue=0;        // value read from the pot
+uint8_t data[256] = {'\0'};
+CpuStatus cpu_status = CpuStatus_init_zero;
+bool has_data;
 
-int reachLeft = 0;
-int reachRight = 0;
-
-
-byte bar[8] = {
-  0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b00000
-};
-
-byte emptybar[8] = {
-  0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000
-};
-
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-
-
-volatile int lastEncoded = 0;
-volatile long encoderValue = 0;
-
-
-String cpuTotal;
-String cpu[16];
-String core;
-String memTotal;
-String memAvailable;
-String mem;
-String disk;
-
-String netup;
-String netdw;
-
-String _1v;
-String _3v;
-String _5v;
-String _12v;
-String cpuFan;
-String cpuTemp;
-String mbTemp;
-String gpuTemp;
-
-
-int page = 0;
-int lastPage = 0;
-
-
-int cpuBarlines = 0;
-int cpuEmptybarlines = 20;
-
-
-int memBarlines = 0;
-int memEmptybarlines = 20;
-
-
-float PercToBar()
-{
-
-  char floatbuf[32];
-  cpuTotal.toCharArray(floatbuf, sizeof(floatbuf));
-  float cpuTotal2 = atof(floatbuf); 
-
-  cpuBarlines = int(float(cpuTotal2) /10 * 2);
-  cpuEmptybarlines = 20 - cpuBarlines;
-
-
-  char floatbuf2[32];
-  mem.toCharArray(floatbuf2, sizeof(floatbuf2));
-  float mem2 = atof(floatbuf2); 
-
-  memBarlines = int(float(mem2) /10 * 2);
-  memEmptybarlines = 20 - memBarlines;
-
-  //Serial.println(barlines);
-  //Serial.println(emptybarlines);
-
-  //screen->setCursor(6, 1);
-  //screen->print(char(0));
-
-}
-
-void StoreData()
-{
-  // print the string when a newline arrives:
-  if (stringComplete) {
-
-    if(inputString.indexOf("CORE") >= 0){
-      core = inputString.substring(4,14);
-    }
-
-    else if(inputString.indexOf("CPU1") >= 0){
-      cpu[0] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU2") >= 0){
-      cpu[1] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU3") >= 0){
-      cpu[2] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU4") >= 0){
-      cpu[3] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU5") >= 0){
-      cpu[4] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU6") >= 0){
-      cpu[5] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU7") >= 0){
-      cpu[6] = inputString.substring(5,15);
-    }
-    else if(inputString.indexOf("CPU8") >= 0){
-      cpu[7] = inputString.substring(5,15);
-    }
-
-    else if(inputString.indexOf("CPUTOTAL") >= 0){
-      cpuTotal = inputString.substring(8,18);
-    }
-
-    else if(inputString.indexOf("MEMTOTAL") >= 0){
-      memTotal = inputString.substring(8,18);
-    }
-    else if(inputString.indexOf("MEMAVAIL") >= 0){
-      memAvailable = inputString.substring(8,18);
-    }
-    else if(inputString.indexOf("MEMPERC") >= 0){
-      mem = inputString.substring(7,17);
-    }
-
-    else if(inputString.indexOf("DISK") >= 0){
-      disk = inputString.substring(4,14);
-    }
-
-    else if(inputString.indexOf("CPUTEMP") >= 0){
-      cpuTemp = inputString.substring(7,17);
-    }
-    else if(inputString.indexOf("MBTEMP") >= 0){
-      mbTemp = inputString.substring(6,17);
-    }
-    else if(inputString.indexOf("GPUTEMP") >= 0){
-      gpuTemp = inputString.substring(7,17);
-    }
-    else if(inputString.indexOf("CPUFAN") >= 0){
-      cpuFan = inputString.substring(6,16);
-    }
-
-    else if(inputString.indexOf("1V") >= 0){
-      _1v = inputString.substring(2,12);
-    }
-    else if(inputString.indexOf("3V") >= 0){
-      _3v = inputString.substring(2,12);
-    }
-    else if(inputString.indexOf("5V") >= 0){
-      _5v = inputString.substring(2,12);
-    }
-    else if(inputString.indexOf("12V") >= 0){
-      _12v = inputString.substring(3,13);
-    }
-    
-
-    else if(inputString.indexOf("NETUP") >= 0){
-      netup = inputString.substring(5);
-    }
-    else if(inputString.indexOf("NETDW") >= 0){
-      netdw = inputString.substring(5);
-    }    
-    
-    
-
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  }
-}
-
-void DisplayData()
-{ 
-  //             PAGE 0             //
-  if (page == 0) {
-    if (lastPage != 0){
-      screen->clear();
-    }
-    lastPage = 0;
-    screen->home (); // go home
-    screen->print(F(" TurBo Monitor 3000  RealTime CPU & MEM "));
-    screen->setCursor (0, 2);
-    screen->print(F(" 2014 - 2018              Display       "));
-    screen->setCursor(38,1);
-    screen->print(F("P1"));
-  }
-  //             PAGE 1             //
-  else if (page == 1) {
-    if (lastPage != 1){
-      screen->clear();
-    }
-    lastPage = 1;
-
-    screen->home (); // go home
-    screen->print(F("CORES:"));
-    screen->setCursor (6, 0);
-    screen->print(core);
-
-    screen->setCursor(8, 0);
-    screen->print(F("C1:"));
-    screen->setCursor (11, 0);
-    screen->print(cpu[0]);
-
-    screen->setCursor(16, 0);
-    screen->print(F("C2:"));
-    screen->setCursor (19, 0);
-    screen->print(cpu[1]); 
-
-    screen->setCursor(24, 0);
-    screen->print(F("C3:"));
-    screen->setCursor (27, 0);
-    screen->print(cpu[2]);
-
-    screen->setCursor(32, 0);
-    screen->print(F("C4:"));
-    screen->setCursor (35, 0);
-    screen->print(cpu[3]);
-
-    screen->setCursor(0, 1);
-    screen->print(F("USAGE:"));
-    screen->setCursor(6, 1);
-    screen->print(cpuTotal);
-
-    screen->setCursor(12, 1);
-    screen->print(F("|"));
-    screen->setCursor(33, 1);
-    screen->print(F("|"));
-
-    int j = 0;
-    for (int i = 0; i < cpuBarlines; i ++){
-      screen->setCursor(i+13,1);
-      screen->print(char(0));
-      j ++;
-    }
-    for ( int k = 0; k < cpuEmptybarlines; k ++){
-      screen->setCursor(k+j+13,1);
-      screen->print(char(1));
-    }
-
-    screen->setCursor(38,1);
-    screen->print(F("P2"));
-  }
-  //             PAGE 2             //
-  else if (page == 2){
-    if (lastPage != 2){
-      screen->clear();
-      lastPage = 2;
-    }
-
-
-    screen->setCursor(0, 0);
-    screen->print(F("MEMTOTAL:"));
-    screen->setCursor (9, 0);
-    screen->print(memTotal);
-
-    screen->setCursor(20, 0);
-    screen->print(F("MEMAVAIL:"));
-    screen->setCursor (29, 0);
-    screen->print(memAvailable);
-
-    screen->setCursor(0, 1);
-    screen->print(F("MEM:"));
-    screen->setCursor (4, 1);
-    screen->print(mem);
-
-    screen->setCursor(12, 1);
-    screen->print(F("|"));
-    screen->setCursor(33, 1);
-    screen->print(F("|"));
-
-    int j = 0;
-    for (int i = 0; i < memBarlines; i ++){
-      screen->setCursor(i+13,1);
-      screen->print(char(0));
-      j ++;
-    }
-    for ( int k = 0; k < memEmptybarlines; k ++){
-      screen->setCursor(k+j+13,1);
-      screen->print(char(1));
-    }
-
-    screen->setCursor(38,1);
-    screen->print(F("P3"));
-  }
-  //             PAGE 3             //
-  else if (page == 3){
-    if (lastPage != 3){
-      screen->clear();
-      lastPage = 3;
-    }
-    screen->setCursor(0,0);
-
-    screen->setCursor(0, 0);
-    screen->print(F("DISK:"));
-    screen->setCursor (5, 0);
-    screen->print(disk);
-
-    screen->setCursor(38,1);
-    screen->print(F("P4"));
-  }
-  //             PAGE 4             //
-  else if (page == 4){
-    if (lastPage != 4){
-      screen->clear();
-      lastPage = 4;
-    }
-    screen->setCursor(0,0);
-
-    screen->setCursor(0, 0);
-    screen->print(F("NETUP:"));
-    screen->setCursor (6, 0);
-    screen->print(netup);
-
-    screen->setCursor(0, 1);
-    screen->print(F("NETDW:"));
-    screen->setCursor (6, 1);
-    screen->print(netdw);
-    
-    screen->setCursor(38,1);
-    screen->print(F("P5"));
-  }
-  //             PAGE 5             //
-  else if (page == 5){
-    if (lastPage != 5){
-      screen->clear();
-      lastPage = 5;
-    }
-    screen->setCursor(0,0);
-
-    screen->setCursor(0, 0);
-    screen->print(F("TEMP"));
-
-    screen->setCursor(5,0);
-    screen->print(F("CPU:"));
-
-    screen->setCursor(9,0);
-    screen->print(cpuTemp);
-
-    screen->setCursor(14,0);
-    screen->print(F("MB:"));
-
-    screen->setCursor(17,0);
-    screen->print(mbTemp);
-
-    screen->setCursor(22,0);
-    screen->print(F("GPU:"));
-
-    screen->setCursor(26,0);
-    screen->print(gpuTemp);
-
-    screen->setCursor(31,0);
-    screen->print(F("FAN:"));
-
-    screen->setCursor(35,0);
-    screen->print(cpuFan);
-
-
-    screen->setCursor(0,1);
-    screen->print(F("1V:"));
-
-    screen->setCursor(3,1);
-    screen->print(_1v);
-
-    screen->setCursor(9,1);
-    screen->print(F("3V:"));
-
-    screen->setCursor(12,1);
-    screen->print(_3v);
-
-    screen->setCursor(18,1);
-    screen->print(F("5V:"));
-
-    screen->setCursor(21,1);
-    screen->print(_5v);
-
-    screen->setCursor(27,1);
-    screen->print(F("12V:"));
-
-    screen->setCursor(31,1);
-    screen->print(_12v);
-
-
-    screen->setCursor(38,1);
-    screen->print(F("P6"));
-  }
-}
-
-void CheckDir(){
-  int val = analogRead(analogInPin1); 
-  if (reachLeft == 0){
-    if (val < 100){
-      //Serial.println("Left");
-      page --  ;
-      if (page < 0){
-        page = 0;
-      }
-      reachLeft = 1;
-    }
-  }
-  if (val > 100){
-    reachLeft = 0;
-  }
-
-  if (reachRight == 0){
-    if (val > 800){
-      //Serial.println("Right");
-      page ++;
-      if (page > 5){
-        page = 5;
-      }
-      reachRight = 1;
-    }
-  }
-  if (val < 800){
-    reachRight = 0;
-  }
-
-}
 
 void setup()
 {
-  screen->begin (40,2);
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
 
-  cpuTotal.reserve(16);
-  cpu[16].reserve(16);
-  core.reserve(16);
-
-  memTotal.reserve(16);
-  memAvailable.reserve(16);
-  mem.reserve(16);
-
-  disk.reserve(16);
-
-  _1v.reserve(16);
-  _3v.reserve(16);
-  _5v.reserve(16);
-  _12v.reserve(16);
-
-  cpuFan.reserve(16);
-  cpuTemp.reserve(16);
-  mbTemp.reserve(16);
-  gpuTemp.reserve(16);
-
-  //pinMode(encoderPin1, INPUT); 
-  //pinMode(encoderPin2, INPUT);
-
-  //digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
-  //digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
-
-  //call updateEncoder() when any high/low changed seen
-  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
-  //attachInterrupt(0, updateEncoder, CHANGE); 
-  //attachInterrupt(1, updateEncoder, CHANGE);
-
-  screen->createChar(0, bar);
-  screen->createChar(1, emptybar);
-
-  // Switch on the backlight
-  screen->clear();
-  screen->home (); // go home
-  screen->print(" TurBo Monitor 3000  RealTime CPU & MEM ");
-  screen->setCursor (0, 2);
-  screen->print(" 2014                     Display       ");
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(1);
+  tft.println(F("TurBo Monitor 3000"));
+  tft.print(F("RealTime CPU & MEM Display   2020"));
   delay(1000);
-  screen->clear();
-  
+  tft.fillScreen(ILI9341_BLACK);
 }
 
 void loop()
 {
-  CheckDir();
-  StoreData();
-  PercToBar();
-  DisplayData();
+  if (has_data) {
+    pb_istream_t stream = pb_istream_from_buffer(data, sizeof(data));
+    bool status = pb_decode(&stream, CpuStatus_fields, &cpu_status);
+
+    tft.setCursor (0, 0);
+    
+    if (!status) {
+      tft.print("Cores: ");
+      tft.println(cpu_status.cores);
+      
+      tft.print("Total: ");
+      tft.println(cpu_status.cpuTotal);
+      
+      tft.print("Core 00: ");
+      tft.println(cpu_status.core0);
+      
+      tft.print("Core 01: ");
+      tft.println(cpu_status.core1);
+      
+      tft.print("Core 02: ");
+      tft.println(cpu_status.core2);
+      
+      tft.print("Core 03: ");
+      tft.println(cpu_status.core3);
+      
+      tft.print("Core 04: ");
+      tft.println(cpu_status.core4);
+      
+      tft.print("Core 05: ");
+      tft.println(cpu_status.core5);
+      
+      tft.print("Core 06: ");
+      tft.println(cpu_status.core6);
+      
+      tft.print("Core 07: ");
+      tft.println(cpu_status.core7);
+      
+      tft.print("Core 08: ");
+      tft.println(cpu_status.core8);
+      
+      tft.print("Core 09: ");
+      tft.println(cpu_status.core9);
+      
+      tft.print("Core 10: ");
+      tft.println(cpu_status.core10);
+      
+      tft.print("Core 11: ");
+      tft.println(cpu_status.core11);
+      
+      tft.print("RAM Memory: ");
+      tft.println(cpu_status.mem_percent);
+      
+      tft.print("RAM Total: ");
+      tft.println(cpu_status.mem_total);
+      
+      tft.print("RAM Available: ");
+      tft.println(cpu_status.mem_available);
+      
+      tft.print("Disk: ");
+      tft.println(cpu_status.disk_percent);
+      
+      tft.print("Network UP: ");
+      tft.println(cpu_status.net_up);
+      
+      tft.print("Network DOWN: ");
+      tft.println(cpu_status.net_dw);
+    }
+    else {
+      tft.print("ERROR");
+    }
+  }
 }
 
 void serialEvent()
 {
+  int index = 0;
   while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read(); 
-    // add it to the inputString:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-    else {
 
-      inputString += inChar;
-    }
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
+    data[index] = Serial.read();
+    index++;
   }
+  has_data = true;
 }
